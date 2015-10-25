6e4b5134edd46cfafbbd5599fdb018e6c2848b1b
ihmayor <ihmayor@ucalgary.ca>
2015-10-25 10:47:53 -0600

    Clean Up on buttons

--- SushiProject/obj/Debug/SushiSushi_Content.g.i.cs
+++ SushiProject/obj/Debug/SushiSushi_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/special.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/appetizer1.png")]
 
 
